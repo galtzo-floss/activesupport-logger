ef4f4c7ccc629c2407dea3a2975d15631d9ad3b5
Peter H. Boling <peter.boling@gmail.com>
2026-08-04 00:12:51 -0600

    🎨 Template bootstrap by kettle-jem v7.1.0

--- lib/activesupport/logger/version.rb
+++ lib/activesupport/logger/version.rb
@@ -1,13 +1,15 @@
 # frozen_string_literal: true
 
-module Activesupport
+module ActiveSupport
   module Logger
-    # Version namespace for this gem.
-    module Version
-      # Current gem version.
-      VERSION = "2.0.3"
+    module SimpleFormatter
+      # Version namespace for this gem.
+      module Version
+        # Current gem version.
+        VERSION = "2.0.3"
+      end
+      # Current gem version exposed at the traditional constant location.
+      VERSION = Version::VERSION # Traditional Constant Location
     end
-    # Current gem version exposed at the traditional constant location.
-    VERSION = Version::VERSION # Traditional Constant Location
   end
 end
