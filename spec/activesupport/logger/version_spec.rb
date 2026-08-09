21f26f5acee2923cd711b1b9f71c72acf8a2aa08
Peter H. Boling <peter.boling@gmail.com>
2026-08-08 23:45:13 -0600

    Cover dependency-free gemspec version loading

--- spec/activesupport/logger/version_spec.rb
+++ spec/activesupport/logger/version_spec.rb
@@ -12,4 +12,17 @@ RSpec.describe ActiveSupport::Logger::SimpleFormatter::Version do
 
     expect(anonymous_namespace::ActiveSupport::Logger::SimpleFormatter::Version::VERSION).to eq(described_class::VERSION)
   end
+
+  it "loads without the runtime logger dependency for gemspec evaluation" do
+    version_path = File.expand_path("../../../lib/activesupport/logger/version.rb", __dir__)
+    logger = Object.send(:remove_const, :Logger)
+    anonymous_namespace = Module.new
+
+    Kernel.load(version_path, anonymous_namespace)
+
+    version = anonymous_namespace::ActiveSupport::Logger::SimpleFormatter::Version::VERSION
+    expect(version).to eq(described_class::VERSION)
+  ensure
+    Object.const_set(:Logger, logger) if logger
+  end
 end
